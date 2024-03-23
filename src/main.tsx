132e9a4ede4bbddbec5914fb61aa232d2cad9bf7
FabianReitz <reitz@stadtwerk.org>
2024-03-23 01:12:22 +0100

    🎉 Start new website design

--- src/main.tsx
+++ src/main.tsx
@@ -3,10 +3,12 @@ import './index.css';
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 
-import { App } from './App.tsx';
+import { BetaBanner } from './components/BetaBanner/BetaBanner';
+import { Navbar } from './components/Navbar/Navbar';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <App />
+    <BetaBanner />
+    <Navbar />
   </React.StrictMode>
 );
